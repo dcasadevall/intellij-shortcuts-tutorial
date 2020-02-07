808eafb6988b0f68611c6acf4642df9121688b2f
Daniel Casadevall Pino <dpino@nianticlabs.com>
2020-02-07 10:01:51 -0800

    Provide task executor

--- src/main/java/interfaces/TaskModule.java
+++ src/main/java/interfaces/TaskModule.java
@@ -1,7 +1,20 @@
 package main.java.interfaces;
 
 import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
 
 @Module(library = true)
 public class TaskModule {
+  @Singleton
+  @Provides
+  TaskExecutor provideTaskExecutor(ProxyingTaskExecutor proxyingTaskExecutor) {
+    return proxyingTaskExecutor;
+  }
+
+  @Singleton
+  @Provides
+  ProxyingTaskExecutor provideProxyingTaskExecutor(NoopTaskExecutor noopTaskExecutor) {
+    return new ProxyingTaskExecutor(noopTaskExecutor);
+  }
 }
