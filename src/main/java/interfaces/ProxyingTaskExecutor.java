4156d6f64ce0422f1535175979fe377fa84d160a
Daniel Casadevall Pino <dpino@nianticlabs.com>
2020-02-07 09:10:48 -0800

    Implemented delegate

--- src/main/java/interfaces/ProxyingTaskExecutor.java
+++ src/main/java/interfaces/ProxyingTaskExecutor.java
@@ -1,4 +1,19 @@
 package main.java.interfaces;
 
 public class ProxyingTaskExecutor implements TaskExecutor {
+  private final TaskExecutor taskExecutor;
+
+  public ProxyingTaskExecutor(TaskExecutor taskExecutor) {
+    this.taskExecutor = taskExecutor;
+  }
+
+  @Override
+  public void Execute() {
+    taskExecutor.Execute();
+  }
+
+  @Override
+  public void Cancel() {
+    taskExecutor.Cancel();
+  }
 }
