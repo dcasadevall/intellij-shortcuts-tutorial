385f246b81ee4a9fe343e7b977cbfbb0548637d5
Daniel Casadevall Pino <dpino@nianticlabs.com>
2020-02-07 09:21:01 -0800

    Builder object created

--- src/main/java/interfaces/Task.java
+++ src/main/java/interfaces/Task.java
@@ -1,6 +1,37 @@
 package main.java.interfaces;
 
 public class Task {
-  public String name;
-  public int priority;
+  private String name;
+  private int priority;
+
+  private Task(Builder builder) {
+    name = builder.name;
+    priority = builder.priority;
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+
+  public static final class Builder {
+    private String name;
+    private int priority;
+
+    private Builder() {
+    }
+
+    public Builder withName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder withPriority(int priority) {
+      this.priority = priority;
+      return this;
+    }
+
+    public Task build() {
+      return new Task(this);
+    }
+  }
 }
